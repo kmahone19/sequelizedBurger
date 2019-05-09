148bdd6afa45f7da2ca91d38ea13e60947455dee
Kevin M <kevin.mahone19@gmail.com>
2019-05-09 09:54:29 -0400

    minnor fixes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -5,38 +5,36 @@ var db = require("../models");
 module.exports = app => {
 
   // route to pull all info 
-  app.get("/api/burgers", function(req, res){
+  app.get("/api/burgers", function (req, res) {
     db.Burger.Findall({})
-    .then(dbBurgerData => res.json(dbBurgerData))
-    .catch(err => {
-      console.log(err);
-      res.jason(err);
-    });
+      .then(dbBurgerData => res.json(dbBurgerData))
+      .catch(err => {
+        console.log(err);
+        res.jason(err);
+      });
   });
 
   // route to add new burgers
-  app.post("/api/burgers", function(req, res){
+  app.post("/api/burgers", function (req, res) {
     db.Burger.create(req.body)
-    .then(dbBurgerData => res.json(dbBurgerData))
-    .catch(err => {
-      console.log(err);
-      res.jason(err);
-    });
+      .then(dbBurgerData => res.json(dbBurgerData))
+      .catch(err => {
+        console.log(err);
+        res.jason(err);
+      });
   })
   // route to update exsisting burgers
-  app.put("/api/burgers/:id", function(req, res){
-   db.Burger.update(req.body,{
-     where: {
-       where:{
-         id: req.body.id
-       }
-     }
-   })
+  app.put("/api/burgers/:id", function (req, res) {
+    db.Burger.update( {devoured: req.body.devoured}, {
+        where: {
+          id: req.params.id
+        }
+      })
       .then(dbBurgerData => res.json(dbBurgerData))
       .catch(err => {
         console.log(err);
         res.json(err);
       });
   });
-  
+
 }
